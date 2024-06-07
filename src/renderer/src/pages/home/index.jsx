9afe6d718b42534521d03e9da224243b31a3dac1
Elias Treadway <eliastreadwayofficial@gmail.com>
2024-06-07 17:40:29 -0400

    Window Size Changes

--- src/renderer/src/pages/home/index.jsx
+++ src/renderer/src/pages/home/index.jsx
@@ -1,14 +1,15 @@
 import { useContext, useEffect, useState } from 'react';
 import styles from './home.module.css';
+import albumCover from '../../assets/cover.jpeg';
 import { AuthContext } from '../../contexts/auth';
 
 
 const Home = () => {
 
   const { getAuthToken, accessCode } = useContext(AuthContext);
-  
+
   useEffect(() => {
-  
+
     getAuthToken();
 
     console.log(accessCode);
@@ -16,39 +17,75 @@ const Home = () => {
   }, [accessCode.length == 0]);
 
   useEffect(() => {
-  }, [])
 
+    const dropOne = document.getElementById('dropOne');
+    const resizableElement = document.getElementById('dropTwo');
+    const resizeHandle = document.getElementById('resizeHandle');
+
+    let isResizing = false;
+
+    resizeHandle.addEventListener('mousedown', (e) => {
+      isResizing = true;
+
+      const onMouseMove = (e) => {
+        if (!isResizing) return;
+
+        const rectWidth = resizableElement.getBoundingClientRect().left;
+        const rectHeight = resizableElement.getBoundingClientRect().bottom;
+
+        const newWidth = e.clientX - rectWidth;
+      
+        const newHeight = (window.innerHeight - e.clientY) - (window.innerHeight - rectHeight);
+
+        console.log({bottomHeight: rectHeight, windowHeight: window.innerHeight, diff: (window.innerHeight - rectHeight), newHeight: newHeight});
+
+        resizableElement.style.width = `${newWidth}px`;
+
+        dropOne.style.height = "calc(100% - "+ newHeight + 'px';
+        resizableElement.style.height = `${newHeight}px`;
+      };
+
+      const onMouseUp = () => {
+        isResizing = false;
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+      };
+
+      window.addEventListener('mousemove', onMouseMove);
+      window.addEventListener('mouseup', onMouseUp);
+    });
+  });
 
   return (
     <div className={styles.homeContainer}>
       <div className={styles.container}>
-        <div className={styles.column}>
-          <div className={styles.dropOne}>
-            <div className={styles.albumCover} />
+        <div className={styles.left_content}>
+          <div className={styles.column}>
+            <div id='dropOne' className={styles.dropOne} style={{ backgroundImage: `url(${albumCover})` }} />
+            <div id='dropTwo' className={styles.dropTwo}>
+              <div className={styles.content}>
+                <h1>Get Your Wish</h1>
+                <h3 style={{ paddingLeft: "1px" }}>Porter Robinson</h3>
+                <p style={{ paddingLeft: "1px", marginTop: "10px" }}>Benjamin Schiff Platt born September 24, 1993 is an American actor, singer, and songwriter. Platt, the son of film and theater producer Marc Platt, began his acting career in musical theater as a child and appeared in productions of The Sound of Music 2006 and The Book of Mormon 2012-2015, rising to prominence for originating the title role in Broadway coming-of-age musical Dear Evan Hansen 2015-2017.</p>
+                <h2 style={{ marginTop: "15px" }}>Performed By</h2>
+                <p style={{ paddingLeft: "1px" }}>Porter Robinson</p>
+                <h2 style={{ marginTop: "15px" }}>Written By</h2>
+                <p style={{ paddingLeft: "1px" }}>Porter Robinson</p>
+              </div>
+              <div id='resizeHandle' class={styles.resizeHandle} />
+            </div>
           </div>
-          <div style={{ height: "15px" }} />
-          <div className={styles.songDescription}>
-            <div className={styles.content}>
-              <h1>Get Your Wish</h1>
-              <h3 style={{ paddingLeft: "1px" }}>Porter Robinson</h3>
-              <p style={{ paddingLeft: "1px", marginTop: "10px" }}>Benjamin Schiff Platt born September 24, 1993 is an American actor, singer, and songwriter. Platt, the son of film and theater producer Marc Platt, began his acting career in musical theater as a child and appeared in productions of The Sound of Music 2006 and The Book of Mormon 2012-2015, rising to prominence for originating the title role in Broadway coming-of-age musical Dear Evan Hansen 2015-2017.</p>
-              <h2 style={{  marginTop: "15px" }}>Performed By</h2>
-              <p style={{ paddingLeft: "1px" }}>Porter Robinson</p>
-              <h2 style={{  marginTop: "15px" }}>Written By</h2>
-              <p style={{ paddingLeft: "1px" }}>Porter Robinson</p>
+          <div className={styles.columnTwo}>
+            <div className={styles.main}>
+
+            </div>
+            <div style={{ height: "15px" }} />
+            <div className={styles.controls}>
+
             </div>
           </div>
         </div>
-        <div className={styles.columnTwo}>
-          <div className={styles.main}>
-
-          </div>
-          <div style={{ height: "15px"}} />
-          <div className={styles.controls}>
-
-          </div>
-        </div>
-        <div className={styles.column}>
+        <div className={styles.columnThree}>
           <div className={styles.playlist}>
 
           </div>

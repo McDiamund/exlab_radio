1a60bd7af024960a0d5b97eb7e8d4381567f1e1a
Elias Treadway <eliastreadwayofficial@gmail.com>
2024-06-11 17:53:15 -0400

    Window Expansion + Spotify Webplayback changes

--- src/renderer/src/pages/home/index.jsx
+++ src/renderer/src/pages/home/index.jsx
@@ -4,6 +4,7 @@ import albumCover from '../../assets/cover.jpeg';
 import { AuthContext } from '../../contexts/auth';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import AddIcon from '@mui/icons-material/Add';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 
 
@@ -20,7 +21,7 @@ const Home = () => {
 
   }, [accessCode.length == 0]);
 
-  useEffect(() => {
+  function dragBottomLeftSection() {
 
     const dropOne = document.getElementById('dropOne');
     const resizableElement = document.getElementById('dropTwo');
@@ -58,6 +59,55 @@ const Home = () => {
       window.addEventListener('mousemove', onMouseMove);
       window.addEventListener('mouseup', onMouseUp);
     });
+
+  }
+
+  function dragCenterSection() {
+
+    const controls = document.getElementById('controls');
+    const resizableElement = document.getElementById('main');
+    const resizeHandle = document.getElementById('resizeHandleCenter');
+
+    let isResizing = false;
+
+    resizeHandle.addEventListener('mousedown', (e) => {
+      isResizing = true;
+
+      const onMouseMove = (e) => {
+        if (!isResizing) return;
+
+        const rectWidth = resizableElement.getBoundingClientRect().left;
+        const rectHeight = resizableElement.getBoundingClientRect().top;
+
+        const newWidth = e.clientX - rectWidth;
+      
+        const newHeight = (e.clientY);
+
+        console.log({bottomHeight: rectHeight, windowHeight: window.innerHeight, diff: (window.innerHeight - rectHeight), newHeight: newHeight});
+
+        resizableElement.style.width = `${newWidth}px`;
+
+        controls.style.height = "calc(100% - "+ newHeight + 'px';
+        resizableElement.style.height = `${newHeight}px`;
+      };
+
+      const onMouseUp = () => {
+        isResizing = false;
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+      };
+
+      window.addEventListener('mousemove', onMouseMove);
+      window.addEventListener('mouseup', onMouseUp);
+    });
+
+  }
+
+  useEffect(() => {
+
+    dragBottomLeftSection();
+    dragCenterSection();
+    
   }, []);
 
   return (
@@ -76,26 +126,46 @@ const Home = () => {
                 <h2 style={{ marginTop: "15px" }}>Written By</h2>
                 <p style={{ paddingLeft: "1px" }}>Porter Robinson</p>
               </div>
-              <div id='resizeHandle' class={styles.resizeHandle} />
+              <ArrowOutwardIcon id="resizeHandle" className={styles.resizeHandle} style={{ color: "white", marginRight: "-7px", marginTop: "-6px"}} />
             </div>
           </div>
           <div className={styles.columnTwo}>
-            <div className={styles.main}>
+            <div id="main" className={styles.main}>
               <div className={styles.title_bar}>
                 <h2 style={{ paddingLeft: "1px" }}>Playlists</h2>
                 <div style={{ flexGrow: 1 }} />'
-                <FormatListBulletedIcon style={{ color: "white", fontSize: "30px" }}/>
-                <AddIcon style={{ color: "white", fontSize: "30px" }} />
+                <FormatListBulletedIcon style={{ color: "white", fontSize: "40px" }}/>
+                <AddIcon style={{ color: "white", fontSize: "40px" }} />
               </div>
               <div className={styles.main_playlist_covers}>
                 <div className={styles.cover} />
                 <div className={styles.cover} />
                 <div className={styles.cover} />
-           
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover} />
+                <div className={styles.cover}>
+                  <AddIcon style={{ color: "white", fontSize: "50px" }} />
+                </div>
+              </div>
+              <div style={{ flexGrow: 1 }} />
+              <div style={{ display: "flex", justifyContent: "right" }}>
+                <ArrowOutwardIcon id="resizeHandleCenter" className={styles.resizeHandle} style={{ color: "white", transform: "rotate(90deg)", marginRight: "-15px", marginBottom: "-10px"}} />
               </div>
             </div>
-            <div style={{ height: "15px" }} />
-            <div className={styles.controls}>
+            <div id="controls" className={styles.controls}>
 
             </div>
           </div>
